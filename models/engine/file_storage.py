cff78d9d1e3d5fbdc13abb084c02ba569dce579c
youssefbassem42 <youssefbassem42@gmail.com>
2024-02-22 00:16:27 +0200

    chmod ++

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,13 +45,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
     def delete(self, obj=None):
         """Delete an Object From FileStorage"""
         if obj in self.__objects:
             del self.__objects[obj.__class__.__name__ + "." + obj.id]
         elif obj is None:
             return
-        
