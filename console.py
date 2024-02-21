ac51668bbd8bb941537516af6adc15cd03ff258b
youssefbassem42 <youssefbassem42@gmail.com>
2024-02-21 23:57:50 +0200

    i hope to finish

--- console.py
+++ console.py
@@ -2,6 +2,10 @@
 """ Console Module """
 import cmd
 import sys
+import re
+import os
+import uuid
+from datetime import datetime
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
