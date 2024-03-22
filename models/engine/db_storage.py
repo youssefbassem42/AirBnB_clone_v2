4d210fff075b059895ea09e155f75c35438a01c8
youssefbassem42 <youssefbassem42@gmail.com>
2024-03-22 15:15:47 -0400

    s

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ new class for sqlAlchemy """
 from os import getenv
-from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.state import State
@@ -68,7 +68,7 @@ class DBStorage:
         """delete an element in the table
         """
         if obj:
-            self.session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
         """configuration
--- models/state.py
+++ models/state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This is the state class"""
+import shlex
+from models.city import City
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel, Base
 import models
-from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
