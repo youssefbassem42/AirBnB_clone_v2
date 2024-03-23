df1489fb1923953b516e68d914d700ee22f5f89e
youssefbassem42 <youssefbassem42@gmail.com>
2024-03-23 06:01:48 -0400

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""" state list task """
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
-from models.state import State
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
 
 
-app.route("/states_list", strict_slashes=False)
-def state_list():
-    """ function to list items from database in html elements"""
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page to display States created"""
     states = storage.all()
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """ function to close the sqlalchemy session"""
+    """Method to remove current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
